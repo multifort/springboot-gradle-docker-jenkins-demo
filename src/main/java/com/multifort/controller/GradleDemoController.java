ca01fe5361613f54d9998732e5d2cbef2f80ed09
lining <191827469@qq.com>
2019-10-23 19:47:34 +0800

    testjenkins

--- src/main/java/com/multifort/controller/GradleDemoController.java
+++ src/main/java/com/multifort/controller/GradleDemoController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class GradleDemoController {
     @GetMapping("/hello")
     public String get(){
-        return "hello";
+        return "hello test jenkins";
     }
 }
